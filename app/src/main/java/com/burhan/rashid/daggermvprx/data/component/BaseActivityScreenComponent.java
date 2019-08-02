027ca4bc9974bee5adfbff50b95677d4ff91da18
Burhanuddin Rashid <burhanrashid5253@gmail.com>
2019-08-02 16:09:58 +0530

    #3 Bind Instance on Main screen

--- app/src/main/java/com/burhan/rashid/daggermvprx/data/component/BaseActivityScreenComponent.java
+++ app/src/main/java/com/burhan/rashid/daggermvprx/data/component/BaseActivityScreenComponent.java
@@ -1,16 +1,29 @@
 package com.burhan.rashid.daggermvprx.data.component;
 
-import com.burhan.rashid.daggermvprx.data.AppScope;
 import com.burhan.rashid.daggermvprx.activities.MainActivity;
-import com.burhan.rashid.daggermvprx.data.module.BaseActivityScreenModule;
+import com.burhan.rashid.daggermvprx.contracter.MainScreenContract;
+import com.burhan.rashid.daggermvprx.data.AppScope;
 
+import dagger.BindsInstance;
 import dagger.Component;
 
 /**
  * Created by Burhanuddin Rashid on 11-May-16.
  */
 @AppScope
-@Component(dependencies = NetComponent.class, modules = BaseActivityScreenModule.class)
+@Component(dependencies = NetComponent.class)
 public interface BaseActivityScreenComponent {
+
     void inject(MainActivity mainActivity);
+
+    @Component.Builder
+    interface Builder {
+
+        BaseActivityScreenComponent build();
+
+        Builder netComponent(NetComponent netComponent);
+
+        @BindsInstance
+        Builder mainView(MainScreenContract.View mainView);
+    }
 }
